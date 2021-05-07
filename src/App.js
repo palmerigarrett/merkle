de641142e6a5bc4f742ca9ecc9b280bb8299d631
palmerigarrett <palmerigarrett@gmail.com>
2021-05-07 17:11:53 -0400

    routing

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Router>
         <div id='container'>
           <Switch>
-            <Route exact path='/merkle/admin'>
+            <Route path='/merkle/admin'>
               <DisplayUsers />
             </Route>
             <Route exact path='/merkle'>
